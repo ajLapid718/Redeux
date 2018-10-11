ff6835775f1b2ad6fc519903d43a6a4fb5e69426
Allan James Lapid <ajLapid718@gmail.com>
2018-10-11 03:40:08 -0400

    Establish Redux store with redux-thunk

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+// Necessities and accessories for constructing our Redux store;
+import { createStore } from 'redux';
+import createLogger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
+
+// Individual reducers altogether under an alias;
+import * as reducers from '../reducers';
+
+// Construct our Redux store;
+const rootReducer = combineReducers(reducers);
+const logger = createLogger({ collapsed: true});
+const middleware = applyMiddleware(thunkMiddleware, logger);
+const store = createStore(rootReducer, middleware);
+
+// Export our store by default, which will be provided to and injected within our entire application;
+export default store;
