8429abadfa8d8937fcfec93f87925ddf4bcdd190
Allan James Lapid <ajLapid718@gmail.com>
2018-10-11 03:45:27 -0400

    Install react-redux and inject Redux store throughout the application

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
