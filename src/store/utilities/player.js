e54b3479da8840ef28d7c6f033a2184516cda061
Allan James Lapid <ajLapid718@gmail.com>
2018-10-12 03:14:45 -0400

    Comment out thunk creators and export action creators

--- src/store/utilities/player.js
+++ src/store/utilities/player.js
@@ -5,31 +5,31 @@ const SET_PLAYER = "SET_PLAYER";
 const REMOVE_PLAYER = "REMOVE_PLAYER";
 
 // ACTION CREATORS;
-const setPlayer = (player) => {
+export const setPlayer = (player) => {
   return {
     type: SET_PLAYER,
     payload: player
   }
 }
 
-const removePlayer = () => {
+export const removePlayer = () => {
   return {
     type: REMOVE_PLAYER
   }
 }
 
-// THUNK CREATORS;
-export const setPlayerThunk = (lastName, firstName) => (dispatch) => {
-  return axios
-    .get(`https://nba-players.herokuapp.com/players-stats/${lastName}/${firstName}`)
-    .then(res => res.data)
-    .then(nbaPlayer => dispatch(setPlayer(nbaPlayer)))
-    .catch(err => console.log(err));
-}
+// // THUNK CREATORS;
+// export const setPlayerThunk = (lastName, firstName) => (dispatch) => {
+//   return axios
+//     .get(`https://nba-players.herokuapp.com/players-stats/${lastName}/${firstName}`)
+//     .then(res => res.data)
+//     .then(nbaPlayer => dispatch(setPlayer(nbaPlayer)))
+//     .catch(err => console.log(err));
+// }
 
-export const removePlayerThunk = () => (dispatch) => {
-  return dispatch(removePlayer());
-}
+// export const removePlayerThunk = () => (dispatch) => {
+//   return dispatch(removePlayer());
+// }
 
 // REDUCER;
 export default (state = {}, action) => {
