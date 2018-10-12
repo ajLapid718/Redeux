49faf8197627f72ed9471da8997ef36761289ad3
Allan James Lapid <ajLapid718@gmail.com>
2018-10-12 03:04:12 -0400

    Set up index file for the root saga

---
+++ src/sagas/index.js
@@ -0,0 +1,1 @@
+// Here, we will import all of our individual sagas, organize them into a single root saga, and then export the collective root saga to be ran elsewhere and used as middleware in our Redux store;
