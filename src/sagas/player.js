16b981c1f0052efd6898f2351eff656e041b2b01
Allan James Lapid <ajLapid718@gmail.com>
2018-10-12 03:35:05 -0400

    Import call, put, and takeLatest redux saga effects

---
+++ src/sagas/player.js
@@ -0,0 +1,14 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+
+
+// export const setPlayerThunk = (lastName, firstName) => (dispatch) => {
+//   return axios
+//     .get(`https://nba-players.herokuapp.com/players-stats/${lastName}/${firstName}`)
+//     .then(res => res.data)
+//     .then(nbaPlayer => dispatch(setPlayer(nbaPlayer)))
+//     .catch(err => console.log(err));
+// }
+
+// export const removePlayerThunk = () => (dispatch) => {
+//   return dispatch(removePlayer());
+// }
