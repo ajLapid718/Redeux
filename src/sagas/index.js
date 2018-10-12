e39571d222fbc1afcec09f0cba1c7c5e4284a7cb
Allan James Lapid <ajLapid718@gmail.com>
2018-10-12 04:22:26 -0400

    Fill out logic for player saga and root saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,1 +1,13 @@
-// Here, we will import all of our individual sagas, organize them into a single root saga, and then export the collective root saga to be ran elsewhere and used as middleware in our Redux store;
+// Here, we will import all of our individual sagas and then organize them into a single root saga;
+import { all } from 'redux-saga/effects';
+
+// Individual sagas;
+import playerSagaWatcher from './player';
+
+// Root saga;
+function* rootSaga() {
+  yield all([...playerSagaWatcher]);
+}
+
+// We then export the collective root saga to be ran elsewhere and used as middleware in our Redux store;
+export default rootSaga;
--- src/sagas/player.js
+++ src/sagas/player.js
@@ -1,14 +1,41 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import { SET_PLAYER, REMOVE_PLAYER, setPlayer, removePlayer } from '../actions';
+import axios from 'axios';
 
-// export const setPlayerThunk = (lastName, firstName) => (dispatch) => {
-//   return axios
-//     .get(`https://nba-players.herokuapp.com/players-stats/${lastName}/${firstName}`)
-//     .then(res => res.data)
-//     .then(nbaPlayer => dispatch(setPlayer(nbaPlayer)))
-//     .catch(err => console.log(err));
-// }
+// Declare asynchronous functions that make API requests and output a Promise for a response;
+function fetchPlayer(lastName, firstName) {
+  return axios
+    .get(
+      `https://nba-players.herokuapp.com/players-stats/${lastName}/${firstName}`
+    )
+    .then(res => res.data);
+}
 
-// export const removePlayerThunk = () => (dispatch) => {
-//   return dispatch(removePlayer());
-// }
+// Declare worker sagas, which will perform API calls when the watcher saga sees the action;
+function* setCurrentPlayer() {
+  try {
+    const nbaPlayer = yield call(fetchPlayer);
+    yield put(setPlayer(nbaPlayer));
+  }
+  catch (err) {
+    console.log(err); // It is better practice to dispatch/put an action where the type signifies an error occurred;
+  }
+}
+
+function* removeCurrentPlayer() {
+  try {
+    yield put(removePlayer());
+  }
+  catch (err) {
+    console.log(err); // It is better practice to dispatch/put an action where the type signifies an error occurred;
+  }
+}
+
+// Normally, we would declare a watcher saga generator function, which watches for actions dispatched to the store and boots up and fires the worker saga;
+// For code splitting purposes, we'll be putting these effects into an array, which will be collectively "yielded" in the root saga;
+const playerSagaWatcher = [
+  takeLatest(SET_PLAYER, setCurrentPlayer),
+  takeLatest(REMOVE_PLAYER, removeCurrentPlayer)
+];
+
+export default playerSagaWatcher;
