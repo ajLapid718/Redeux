925fabb0fce6bf268dde99c9a4b8acb19b56e65d
Allan James Lapid <ajLapid718@gmail.com>
2018-10-16 00:39:11 -0400

    Fill in a preliminary set up of epic middleware for the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
 // Necessities and accessories for constructing our Redux store;
 import { combineReducers, applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
+
+import { createEpicMiddleware } from 'redux-observable';
+/* import epic(s) here
+const epicMiddleware = createEpicMiddleware(importedEpic);
+*/
+
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 // Individual reducers altogether under an alias;
@@ -10,7 +15,7 @@ import * as reducers from '../reducers';
 // Construct our Redux store;
 const rootReducer = combineReducers(reducers);
 const logger = createLogger({ collapsed: true});
-const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware, logger));
+const middleware = composeWithDevTools(applyMiddleware(epicMiddleware, logger));
 const store = createStore(rootReducer, middleware);
 
 // Export our store by default, which will be provided to and injected within our entire application;
