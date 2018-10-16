5337ac08cde6c9b793e0d492eb0d631340025d89
Allan James Lapid <ajLapid718@gmail.com>
2018-10-16 02:49:15 -0400

    Set up root epic with combineEpics

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
 // Necessities and accessories for constructing our Redux store;
 import { combineReducers, applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
-import { createEpicMiddleware } from 'redux-observable';
+import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import { fetchPlayerEpic } from '../epics';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 // Individual reducers altogether under an alias;
 import * as reducers from '../reducers';
+
+// Root epic;
+const rootEpic = combineEpics(fetchPlayerEpic);
 
 // Construct our Redux store;
 const rootReducer = combineReducers(reducers);
@@ -15,7 +18,7 @@ const epicMiddleware = createEpicMiddleware();
 const middleware = composeWithDevTools(applyMiddleware(epicMiddleware, logger));
 const store = createStore(rootReducer, middleware);
 
-epicMiddleware.run(fetchPlayerEpic);
+epicMiddleware.run(rootEpic);
 
 // Export our store by default, which will be provided to and injected within our entire application;
 export default store;
