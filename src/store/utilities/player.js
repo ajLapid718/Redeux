122360ee5ef78074cd93935de8725ce8a5d380c8
Allan James Lapid <ajLapid718@gmail.com>
2018-10-16 01:38:41 -0400

    Remove thunks and set up actions

--- src/store/utilities/player.js
+++ src/store/utilities/player.js
@@ -1,42 +1,46 @@
-import axios from 'axios';
+// ACTION TYPES (FOR ACTION CREATORS AND FOR AN EPIC);
+export const FETCH_PLAYER = "FETCH_PLAYER";
+export const REMOVE_PLAYER = "REMOVE_PLAYER";
 
-// ACTION TYPES;
-const SET_PLAYER = "SET_PLAYER";
-const REMOVE_PLAYER = "REMOVE_PLAYER";
+// ACTION TYPES (FOR ACTION CREATORS AND FOR THE REDUCER);
+const FETCH_PLAYER_SUCCESS = "FETCH_PLAYER_SUCCESS";
+const REMOVE_PLAYER_SUCCESS = "REMOVE_PLAYER_SUCCESS";
 
-// ACTION CREATORS;
-const setPlayer = (player) => {
+// ACTION CREATORS (FOR COMPONENTS);
+export const fetchPlayer = (lastName, firstName) => {
   return {
-    type: SET_PLAYER,
-    payload: player
+    type: FETCH_PLAYER,
+    lastName: lastName,
+    firstName: firstName
   }
 }
 
-const removePlayer = () => {
+export const removePlayer = () => {
   return {
     type: REMOVE_PLAYER
   }
 }
 
-// THUNK CREATORS;
-export const setPlayerThunk = (lastName, firstName) => (dispatch) => {
-  return axios
-    .get(`https://nba-players.herokuapp.com/players-stats/${lastName}/${firstName}`)
-    .then(res => res.data)
-    .then(nbaPlayer => dispatch(setPlayer(nbaPlayer)))
-    .catch(err => console.log(err));
+// ACTION CREATORS (FOR EPIC);
+export const fetchPlayerSuccess = (player) => {
+  return {
+    type: FETCH_PLAYER_SUCCESS,
+    payload: player
+  }
 }
 
-export const removePlayerThunk = () => (dispatch) => {
-  return dispatch(removePlayer());
+export const removePlayerSuccess = () => {
+  return {
+    type: REMOVE_PLAYER_SUCCESS
+  }
 }
 
 // REDUCER;
 export default (state = {}, action) => {
   switch (action.type) {
-    case SET_PLAYER:
+    case FETCH_PLAYER_SUCCESS:
       return action.payload;
-    case REMOVE_PLAYER:
+    case REMOVE_PLAYER_SUCCESS:
       return {};
     default:
       return state;
