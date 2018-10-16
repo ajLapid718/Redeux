1d416654912290c502c29e8e1922fa6c2a48f0f2
Allan James Lapid <ajLapid718@gmail.com>
2018-10-16 03:46:34 -0400

    Refactor working example and prep for pipe refactor

--- src/epics/fetchPlayerEpic.js
+++ src/epics/fetchPlayerEpic.js
@@ -1,12 +1,15 @@
-import { FETCH_PLAYER, getPlayer, fetchPlayerSuccess } from '../store/utilities/player';
-import { Observable } from 'rxjs/Rx';
+import axios from 'axios';
+import { FETCH_PLAYER, fetchPlayerSuccess } from '../store/utilities/player';
+import 'rxjs/Rx';;
+
+const getPlayer = (lastName, firstName) => {
+  return axios.get(`https://nba-players.herokuapp.com/players-stats/${lastName}/${firstName}`);
+};
 
 const fetchPlayerEpic = action$ =>
   action$.ofType(FETCH_PLAYER)
-    .mergeMap(action =>
-      Observable.fromPromise(getPlayer(action.lastName, action.firstName))
+    .mergeMap(action => getPlayer(action.lastName, action.firstName))
         .map(response => fetchPlayerSuccess(response.data))
         .catch(err => console.log(err))
-      )
 
 export default fetchPlayerEpic;
