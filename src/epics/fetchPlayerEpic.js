522b1e112baca0afa88f12b0654c9302256fd615
Allan James Lapid <ajLapid718@gmail.com>
2018-10-17 02:41:50 -0400

    Format the code in the fetchPlayer epic

--- src/epics/fetchPlayerEpic.js
+++ src/epics/fetchPlayerEpic.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 import { FETCH_PLAYER, fetchPlayerSuccess } from '../store/utilities/player';
-import 'rxjs/Rx';;
+import 'rxjs/Rx';
 
 const getPlayer = (lastName, firstName) => {
   return axios.get(`https://nba-players.herokuapp.com/players-stats/${lastName}/${firstName}`);
 };
 
 const fetchPlayerEpic = action$ =>
-  action$.ofType(FETCH_PLAYER)
+  action$
+    .ofType(FETCH_PLAYER)
     .mergeMap(action => getPlayer(action.lastName, action.firstName))
-        .map(response => fetchPlayerSuccess(response.data))
-        .catch(err => console.log(err))
+    .map(response => fetchPlayerSuccess(response.data))
+    .catch(err => console.log(err))
 
 export default fetchPlayerEpic;
