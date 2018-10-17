8d915a78f547435a0c11d29bc6eb8b4c2e19dc00
Allan James Lapid <ajLapid718@gmail.com>
2018-10-17 03:21:26 -0400

    Refactor epics to use pipe function

--- src/epics/fetchPlayerEpic.js
+++ src/epics/fetchPlayerEpic.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
 import { FETCH_PLAYER, fetchPlayerSuccess } from '../store/utilities/player';
-import 'rxjs/Rx';
+import { ofType } from 'redux-observable';
+import { mergeMap, map, catchError } from 'rxjs/operators';
 
 const getPlayer = (lastName, firstName) => {
   return axios.get(`https://nba-players.herokuapp.com/players-stats/${lastName}/${firstName}`);
 };
 
-const fetchPlayerEpic = action$ =>
-  action$
-    .ofType(FETCH_PLAYER)
-    .mergeMap(action => getPlayer(action.lastName, action.firstName))
-    .map(response => fetchPlayerSuccess(response.data))
-    .catch(err => console.log(err))
+const fetchPlayerEpic = action$ => {
+  return action$.pipe(
+    ofType(FETCH_PLAYER),
+    mergeMap(action => getPlayer(action.lastName, action.firstName)),
+    map(response => fetchPlayerSuccess(response.data)),
+    catchError(err => console.log(err)),
+  )
+}
 
 export default fetchPlayerEpic;
--- src/epics/removePlayerEpic.js
+++ src/epics/removePlayerEpic.js
@@ -1,8 +1,13 @@
 import { REMOVE_PLAYER, removePlayerSuccess } from '../store/utilities/player';
+import { ofType } from 'redux-observable';
+import { mapTo, catchError } from 'rxjs/operators';
 
-const removePlayerEpic = action$ =>
-  action$
-    .ofType(REMOVE_PLAYER)
-    .mapTo(removePlayerSuccess());
+const removePlayerEpic = action$ => {
+  return action$.pipe(
+    ofType(REMOVE_PLAYER),
+    mapTo(removePlayerSuccess()),
+    catchError(err => console.log(err))
+  )
+}
 
 export default removePlayerEpic;
