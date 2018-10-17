afec28ba4f225cc58c1790f280c3c04b7ac49fba
Allan James Lapid <ajLapid718@gmail.com>
2018-10-17 02:43:48 -0400

    Format the code in the removePlayer epic

--- src/epics/removePlayerEpic.js
+++ src/epics/removePlayerEpic.js
@@ -1,7 +1,8 @@
 import { REMOVE_PLAYER, removePlayerSuccess } from '../store/utilities/player';
 
 const removePlayerEpic = action$ =>
-  action$.ofType(REMOVE_PLAYER)
+  action$
+    .ofType(REMOVE_PLAYER)
     .mapTo(removePlayerSuccess());
 
 export default removePlayerEpic;
